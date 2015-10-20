8794e45343087b2f4166a2788de85f888ad2d8bf
jefferson <jefferson.poe@gmail.com>
2015-10-20 02:02:50 -0700

    implement dummy data, provide listview, use list adapter, inflate fragment in main activity, specify action bar

--- app/src/main/java/com/pepihome/popmovies/MainActivityFragment.java
+++ app/src/main/java/com/pepihome/popmovies/MainActivityFragment.java
@@ -1,20 +1,42 @@
 package com.pepihome.popmovies;
 
-import android.support.v4.app.Fragment;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
+    private ArrayAdapter<String> mMovieAdapter;
+
     public MainActivityFragment() {
     }
 
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> movieList = new ArrayList<>();
+        String[] dummyTitles = { "Minions", "Superman", "IronMan", "SpiderMan", "Tomb Raider", "Serendipity",
+                "Aeon Flux" };
+        movieList.addAll(Arrays.asList(dummyTitles));
+
+        mMovieAdapter = new ArrayAdapter<>(getContext(), R.layout.list_item_movie, R.id.list_item_textview_movie,
+                movieList);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view_movies);
+        listView.setAdapter(mMovieAdapter);
+
+        return rootView;
+
     }
 }
